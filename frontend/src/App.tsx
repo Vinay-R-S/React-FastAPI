ddcd48932e85d623b0cc3926de77a3107199c453
Vinay-R-S <vinayrsaunshi@gmail.com>
2025-10-18 20:51:31 +0530

    Frontend setup ready

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { Button } from './components/ui/button.tsx'
+
+function App() {
+  return (
+    <>
+      <div className="text-3xl font-bold">
+        Hello world! How are you doing today?
+      </div>
+      <br />
+      <Button>Click Me</Button>
+    </>
+  )
+}
+
+export default App
